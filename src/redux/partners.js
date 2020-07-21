86aa7976acc82d3d525d754b437c7fc2392e7549
Kaneka <kaneka.ky@gmail.com>
2020-07-21 12:49:22 -0700

    Spliting and Combining Reducers with comments

---
+++ src/redux/partners.js
@@ -0,0 +1,8 @@
+import { PARTNERS } from "../shared/partners";
+//1--7
+export const Partners = (state = PARTNERS, action) => {
+    switch (action.type){
+        default:
+            return state; 
+    }
+};
