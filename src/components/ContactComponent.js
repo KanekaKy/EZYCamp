59feb101eccb5662f13f3b9c5448c155b96c77d9
Kaneka <kaneka.ky@gmail.com>
2020-07-22 00:30:53 -0700

    React-redux-form revisited with comments

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -6,7 +6,7 @@ import { Control, Form, Errors, actions } from 'react-redux-form'; //9
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
-const isNumber = val => !isNaN(+val);//7
+const isNumber = val => !isNaN(+val);
 const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
