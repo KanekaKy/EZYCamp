3b3f11abbaeece9a601037cfd2af02846d3c22cf
Kaneka <kaneka.ky@gmail.com>
2020-08-10 21:33:50 -0700

    Week5 task 3 completed

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,7 +3,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderPartner({ partner }) {
     if (partner) {
@@ -20,11 +20,14 @@ function RenderPartner({ partner }) {
 
 
 function PartnerList(props) {
-    const partners= props.partners.partners.map(partner => {
+    const partners = props.partners.partners.map(partner => {
         return (
-            <Media tag="li" key={partner.id} >
-                <RenderPartner partner={partner} />
-            </Media>
+
+            <Fade in key={partner.id}>
+                <Media tag="li">
+                    <RenderPartner partner={partner} />
+                </Media>
+            </Fade>
         );
     });
     if (props.partners.isLoading) {
@@ -42,7 +45,9 @@ function PartnerList(props) {
     return (
         <div className="col-mt-4">
             <Media list>
-                {partners}
+                <Stagger in>
+                    {partners}
+                </Stagger>
             </Media>
         </div>
     )
