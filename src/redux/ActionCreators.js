e267501af3869025cce79438694558cf694cc59b
Kaneka <kaneka.ky@gmail.com>
2020-08-10 21:16:50 -0700

    Week5 Task2

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -85,13 +85,13 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
     return fetch(baseUrl + 'comments', {
         method: "POST",
         body: JSON.stringify(newComment),
-        headers: {//8
+        headers: {
             "Content-Type": "application/json"
         }
     })
         .then(response => {
             if (response.ok) {
-                return response;
+                return alert("Thank you for your feedback");
             } else {
                 const error = new Error(`Error ${response.status}: ${response.statusText}`);
                 error.response = response;
@@ -179,3 +179,33 @@ export const partnersFailed = errMess => ({
     type: ActionTypes.PARTNERS_FAILED,
     payload: errMess
 });
+
+export const postFeedback = (feedback) => () => {
+    return fetch(baseUrl + 'feedback', {
+        method: "POST",
+        body: JSON.stringify(feedback),
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(response => {
+            console.log('Feedback: ', response);
+            alert('Thank you for your feedback!\n' + JSON.stringify(response));
+        })
+        .catch(error => {
+            console.log('Feedback: ', error.message);
+            alert('Your feedback could not be posted\nError: ' + error.message);
+        });
+};
